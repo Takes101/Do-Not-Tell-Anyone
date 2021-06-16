b0136e8a0abeedec738c9c5cd09e58648fff24fc
William Takes <william.e.takes@vanderbilt.edu>
2021-06-15 20:47:09 -0400

    password length criteria

---
+++ script.js
@@ -0,0 +1,31 @@
+// Assignment code here
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// function make for passwrod criteria
+function passwordProcess() {
+  window.alert("Select Password Criteria!");
+  break;
+
+  // password length criteria
+  var passwordLength = window.prompt("Enter Leangth of Password:")
+    console.log(passwordLength);
+
+    if (passwordLength < 8) {
+
+    }
+
+}
